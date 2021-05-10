79ed4095b871b8c8d777da2d8d11cab58534f8ca
root <root@DESKTOP-5JP3J6T.localdomain>
2021-05-10 18:14:54 +0300

    add main

--- Board.cpp
+++ Board.cpp
@@ -17,122 +17,48 @@ namespace ariel
     void Board::set_min_max(unsigned int row, unsigned int col, Direction n, unsigned int len_s)
     {
         //set minimum
-        if (row < min_row)
-        {
-            min_row = row;
-        }
-        if (col < min_col)
-        {
-            min_col = col;
-        }
+        if (row < min_row){min_row = row;}
+        if (col < min_col){min_col = col;}
 
         //set maximum
         if (n == Direction::Vertical)
         {
-            if (row + len_s > max_row)
-            {
-                max_row = row + len_s;
-            }
-            if (col > max_col)
-            {
-                max_col = col;
-            }
+            if (row + len_s > max_row){max_row = row + len_s;}
+            if (col > max_col){max_col = col;}
         }
-        if (n == Direction::Horizontal)
+        else if (n == Direction::Horizontal)
         {
-            if (col + len_s > max_col)
-            {
-                max_col = col + len_s;
-            }
-            if (row > max_row)
-            {
-                max_row = row;
-            }
+            if (col + len_s > max_col){max_col = col + len_s;}
+            if (row > max_row){max_row = row;}
         }
     }
 
-    void Board::set_s_h(unsigned int row, unsigned int col, string s, unsigned int len_s)
+    /* Add the message to board */
+    void Board::add_message(unsigned int row, unsigned int col, Direction d, string &s, unsigned int len_s)
     {
-        for (size_t i = 0; i < len_s; i++)
+        for (char ch : s)
         {
-            board[row][col] = s.at(i);
-            col++;
+            board[row][col] = ch;
+            (d == Direction::Horizontal) ? col++ : row++;
         }
     }
 
-    void Board::set_s_v(unsigned int row, unsigned int col, string s, unsigned int len_s)
-    {
-        for (size_t i = 0; i < len_s; i++)
-        {
-            board[row][col] = s.at(i);
-            row++;
-        }
-    }
-
-    void Board::set_s(unsigned int row, unsigned int col, Direction n, string &s, unsigned int len_s)
-    {
-        if (n == Direction::Horizontal)
-        {
-            set_s_h(row, col, s, len_s);
-        }
-        else
-        {
-            set_s_v(row, col, s, len_s);
-        }
-    }
-
-    bool Board::contain(unsigned int row, unsigned int col)
+    /* true - if contain message in this location, 
+     * false - if not */
+    bool Board::contain_message(unsigned int row, unsigned int col)
     {
         bool ans = false;
         it1 = board.find(row);
         if (it1 != board.end())
         {
             it2 = board[row].find(col);
-            if (it2 != board[row].end())
-            {
-                ans = true;
-            }
+            if (it2 != board[row].end()){ans = true;}
         }
         return ans;
     }
 
-    string Board::read_h(unsigned int row, unsigned int col, unsigned int len)
-    {
-        string ans;
-        for (size_t i = 0; i < len; i++)
-        {
-            if (contain(row, col))
-            {
-                ans += board[row][col];
-            }
-            else
-            {
-                ans += "_";
-            }
-            col++;
-        }
-        return ans;
-    }
-
-    string Board::read_v(unsigned int row, unsigned int col, unsigned int len)
-    {
-        string ans;
-        for (size_t i = 0; i < len; i++)
-        {
-            if (contain(row, col))
-            {
-                ans += board[row][col];
-            }
-            else
-            {
-                ans += "_";
-            }
-            row++;
-        }
-        return ans;
-    }
-
-    unsigned int Board::set_space(unsigned int a)
+    /* return the number of space that need to row in the board */ 
+    unsigned int Board::add_space(unsigned int a)
     {
         unsigned int i = 1;
         while (a / pow(ten, i) >= one)
@@ -142,58 +68,56 @@ namespace ariel
         return i;
     }
 
+    /* post message to the board by location */
     void Board::post(unsigned int row, unsigned int col, Direction n, string s)
     {
         unsigned int len_s = s.size();
         set_min_max(row, col, n, len_s);
-        set_s(row, col, n, s, len_s);
+        add_message(row, col, n, s, len_s);
     }
 
-    string Board::read(unsigned int row, unsigned int col, Direction n, unsigned int len)
+    /* read message from the board by location */
+    string Board::read(unsigned int row, unsigned int col, Direction d, unsigned int len)
     {
         string ans;
-        if (n == Direction::Horizontal)
+        for (size_t i = 0; i < len; i++)
         {
-            ans = read_h(row, col, len);
+            if (contain_message(row, col)){ans += board[row][col];}
+            else{ans += "_";}
+            (d == Direction::Horizontal) ? col++ : row++;
         }
-        else
-        {
-            ans = read_v(row, col, len);
-        }
-
         return ans;
     }
 
     void Board::show()
     {
+        /* print empty board */
         if (board.empty())
         {
             cout << "1: _________\n2: _________\n3: _________\n4: _________\n5: _________" << endl;
             return;
         }
+
+        /* set the maximum and the minimum of the row and the col */
         unsigned int min_r = min(min_row, min_row - one);
         unsigned int min_c = min(min_col, min_col - two);
         unsigned int max_r = max(max_row, max_row + one);
         unsigned int max_c = max(max_col, max_col + two);
+
         string ans;
         for (unsigned int i = min_r; i < max_r; ++i)
         {
-            unsigned int space = set_space(i);
             ans += to_string(i) + ":";
-            for (unsigned int j = space; j < five; ++j)
-            {
-                ans += " ";
-            }
+            
+            /* add space to the row */
+            unsigned int space = add_space(i);
+            for (unsigned int j = space; j < five; ++j){ans += " ";}
+            
+            /* add the rest of the row */
             for (unsigned int j = min_c; j < max_c; ++j)
             {
-                if (contain(i, j))
-                {
-                    ans += board[i][j];
-                }
-                else
-                {
-                    ans += "_";
-                }
+                if (contain_message(i, j)){ans += board[i][j];}
+                else{ans += "_";}
             }
             cout << ans << endl;
             ans = "";
--- Board.hpp
+++ Board.hpp
@@ -15,14 +15,10 @@ namespace ariel
         std::map<unsigned int, char>::iterator it2;
         unsigned int max_row = 0, max_col = 0;
         unsigned int min_row = imax, min_col = imax;
-        void set_s_h(unsigned int row, unsigned int col, std::string s,unsigned int len_s);
         void set_min_max(unsigned int row, unsigned int col, ariel::Direction n,unsigned int len_s);
-        void set_s_v(unsigned int row, unsigned int col, std::string s,unsigned int len_s);
-        void set_s(unsigned int row, unsigned int col, ariel::Direction n, std::string &s,unsigned int len_s);
-        bool contain(unsigned int row, unsigned int col);
-        std::string read_h(unsigned int row, unsigned int col,unsigned int len);
-        std::string read_v(unsigned int row, unsigned int col, unsigned int len);
-        static unsigned int set_space(unsigned int a) ;
+        void add_message(unsigned int row, unsigned int col, ariel::Direction n, std::string &s,unsigned int len_s);
+        bool contain_message(unsigned int row, unsigned int col);
+        static unsigned int add_space(unsigned int a) ;
 
     public:
         void post(unsigned int row, unsigned int column, ariel::Direction n, std::string s);
